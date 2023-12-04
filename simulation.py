f42fb501e1e912722d773077c252a540865c8696
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2023-12-03 20:18:04 -0500

    refactor

---
+++ simulation.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+
+class Time:
+    now = 0
+
+    @staticmethod
+    def incr_time():
+        Time.now += 1
+
+
+class SimulationObject:
+    last_id = -1
+    __objects = {}
+
+    def __init__(self) -> None:
+        self.created_at = Time.now
+        self.id = self.__class__.generate_id()
+        self.global_id = f"{self.__class__.__name__.lower()}{self.id}"
+        SimulationObject.__objects[self.global_id] = self
+
+    def update(self) -> None:
+        pass
+
+    def display_str(self) -> str:
+        return self.global_id
+
+    @classmethod
+    def generate_id(cls) -> int:
+        cls.last_id += 1
+        return cls.last_id
+
+    @staticmethod
+    def get_object(global_id: str) -> SimulationObject:
+        return SimulationObject.__objects.get(global_id, None)
--- trading_objects.py
+++ trading_objects.py
@@ -1,40 +1,7 @@
 from __future__ import annotations
-
 from typing import Union, List, Dict
 
-
-class Time:
-    now = 0
-
-    @staticmethod
-    def incr_time():
-        Time.now += 1
-
-
-class SimulationObject:
-    last_id = -1
-    __objects = {}
-
-    def __init__(self) -> None:
-        self.created_at = Time.now
-        self.id = self.__class__.generate_id()
-        self.global_id = f"{self.__class__.__name__.lower()}{self.id}"
-        SimulationObject.__objects[self.global_id] = self
-
-    def update(self) -> None:
-        pass
-
-    def display_str(self) -> str:
-        return self.global_id
-
-    @classmethod
-    def generate_id(cls) -> int:
-        cls.last_id += 1
-        return cls.last_id
-
-    @staticmethod
-    def get_object(global_id: str) -> SimulationObject:
-        return SimulationObject.__objects.get(global_id, None)
+from simulation import Time, SimulationObject
 
 
 class Order(SimulationObject):
@@ -315,10 +282,12 @@ class Account(SimulationObject):
         self.set_holding(symbol, val=val + self.get_holding(symbol))
 
 
-class Trade:
+class Trade(SimulationObject):
     def __init__(
         self, symbol: str, price: float, size: int, buyer_id: str, seller_id: str
     ) -> None:
+        super().__init__()
+
         self.symbol = symbol
         self.price = price
         self.size = size
