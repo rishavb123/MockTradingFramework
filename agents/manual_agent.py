c074afa8c477ad4bdefed4a019b1321d99959771
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2023-12-04 01:34:33 -0500

    setting up manual agent

--- agents/manual_agent.py
+++ agents/manual_agent.py
@@ -1,8 +1,15 @@
+import pygame
+
 from trading_objects import Agent
 from command_display import CommandDisplay, Command, Argument
 
-class ManualAgent(Agent):
 
+class ManualAgent(Agent):
     def __init__(self) -> None:
         super().__init__()
-        
+        self.gui = CommandDisplay(commands=[
+            Command
+        ], draw_fn=self.visualize)
+
+    def visualize(self, screen: pygame.surface.Surface, x: int, y: int, w: int, h: int):
+        pass
