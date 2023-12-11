ca4187aaa5423f6d7884a33f3d0dac8a5fa8df51
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2023-12-11 01:32:52 -0500

    added market maker

--- mocks/synced_flip_payout/agents.py
+++ mocks/synced_flip_payout/agents.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-from trading_objects import Agent, Exchange, Event
-
+from trading_objects import Agent, Exchange, Event, Account
 from .config import *
 
 
@@ -20,6 +19,7 @@ class RetailInvestor(Agent):
         )
         self.sizing = np.random.randint(*RETAIL_SIZING_RANGE)
         self.resting_order_expire_time = 100
+        self.order_price_margin_to_ensure_lift = 3
 
     def update(self) -> None:
         super().update()
@@ -49,7 +49,8 @@ class RetailInvestor(Agent):
         ):
             self.limit_order(
                 dir=self.opinion,
-                price=cheapest_investment[1],
+                price=cheapest_investment[1]
+                + self.opinion * TICK_SIZE * self.order_price_margin_to_ensure_lift,
                 size=self.sizing,
                 symbol=cheapest_investment[0],
                 frames_to_expire=self.resting_order_expire_time,
@@ -88,6 +89,8 @@ class RetailTrader(Agent):
                 symbol_idx = i
 
         self.symbol = SYMBOLS[symbol_idx]
+        self.resting_order_expire_time = 100
+        self.edge = 5
 
     def update(self) -> None:
         super().update()
@@ -108,7 +111,12 @@ class RetailTrader(Agent):
                 if self.opinion > 0:
                     if ask < self.expected_stock_value and ask - bid < 2 * TICK_SIZE:
                         self.cancel_all_open_orders()
-                        self.bid(ask, ask_order.size, self.symbol, frames_to_expire=100)
+                        self.bid(
+                            ask,
+                            ask_order.size,
+                            self.symbol,
+                            frames_to_expire=self.resting_order_expire_time,
+                        )
                     elif (
                         bid + TICK_SIZE < self.expected_stock_value
                         and bid_order.id not in self.open_orders
@@ -117,12 +125,17 @@ class RetailTrader(Agent):
                             bid + TICK_SIZE,
                             self.sizing,
                             self.symbol,
-                            frames_to_expire=100,
+                            frames_to_expire=self.resting_order_expire_time,
                         )
                 else:
                     if bid > self.expected_stock_value and ask - bid < 2 * TICK_SIZE:
                         self.cancel_all_open_orders()
-                        self.ask(bid, bid_order.size, self.symbol, frames_to_expire=100)
+                        self.ask(
+                            bid,
+                            bid_order.size,
+                            self.symbol,
+                            frames_to_expire=self.resting_order_expire_time,
+                        )
                     elif (
                         ask - TICK_SIZE > self.expected_stock_value
                         and ask_order.id not in self.open_orders
@@ -132,16 +145,23 @@ class RetailTrader(Agent):
                             ask - TICK_SIZE,
                             self.sizing,
                             self.symbol,
-                            frames_to_expire=100,
+                            frames_to_expire=self.resting_order_expire_time,
                         )
-
         else:
             if self.opinion > 0 and len(bids) == 0:
-                self.bid(1, self.sizing, self.symbol, frames_to_expire=100)
+                self.bid(
+                    1,
+                    self.sizing,
+                    self.symbol,
+                    frames_to_expire=self.resting_order_expire_time,
+                )
             elif self.opinion < 0 and len(asks) == 0:
-                self.ask(99, self.sizing, self.symbol, frames_to_expire=100)
-
-        return super().update()
+                self.ask(
+                    99,
+                    self.sizing,
+                    self.symbol,
+                    frames_to_expire=self.resting_order_expire_time,
+                )
 
 
 class HedgeFund(Agent):
@@ -267,12 +287,11 @@ class HedgeFund(Agent):
                 frames_to_expire=self.resting_order_expire_time,
             )
 
-        return super().update()
-
 
 class ArbAgent(Agent):
     def __init__(self) -> None:
         super().__init__()
+        self.margin_to_ensure_trade = 1
 
     def update(self) -> None:
         super().update()
@@ -302,12 +321,14 @@ class ArbAgent(Agent):
                     markets[highest_bid_symbol][1], markets[lowest_ask_symbol][3]
                 )
                 self.ask(
-                    price=markets[highest_bid_symbol][0] - 1,
+                    price=markets[highest_bid_symbol][0]
+                    - self.margin_to_ensure_trade * TICK_SIZE,
                     size=size,
                     symbol=highest_bid_symbol,
                 )
                 self.bid(
-                    price=markets[lowest_ask_symbol][2] + 1,
+                    price=markets[lowest_ask_symbol][2]
+                    + self.margin_to_ensure_trade * TICK_SIZE,
                     size=size,
                     symbol=lowest_ask_symbol,
                 )
@@ -319,51 +340,184 @@ class MarketMaker(Agent):
     def __init__(self) -> None:
         super().__init__()
 
-        self.fair_value = 50  # Calculate a current fair for the stock
-        self.edge = 5  # Place orders around the fair value
+        self.sizing = 10
         self.close_out_positions_at = 50
         self.price_margin_on_close = 5
+        self.resting_order_expiration_time = 2
+        self.min_edge = 5 * TICK_SIZE
+        self.holding_adjustment_mult_range = 0.25
+
+    def register_exchange(self, exchange: Exchange) -> None:
+        super().register_exchange(exchange)
+
+        self.fairs = {k: 50 for k in self.exchange.symbols}
+
+        self.tight_market_mids = {k: [] for k in self.exchange.symbols}
+        self.tight_market_max_spread = 5
+
+    def limit_order(
+        self,
+        dir: int,
+        price: float,
+        size: int,
+        symbol: str | None = None,
+        exchange_name: str | None = None,
+        frames_to_expire: int | None = None,
+    ) -> int:
+        return super().limit_order(
+            dir, price, size, symbol, exchange_name, frames_to_expire
+        )
+
+    def get_total_holding(self) -> int:
+        self.holdings = self.exchange.get_account_holdings(self)
+        total_holding = sum(self.holdings.values()) - self.holdings[Account.CASH_SYM]
+        return total_holding
+
+    def clip(self, x: float) -> float:
+        return min(max(x, 0), MAX_PAYOUT)
 
     def close_positions(self) -> None:
         order_books = self.exchange.public_info()
         self.holdings = self.exchange.get_account_holdings(self)
-
-        total_holding = sum(self.holdings.values())
+        total_holding = self.get_total_holding()
 
         if total_holding == 0:
             self.cancel_all_open_orders()
             return
 
-        for symbol in self.holdings:
-            bids = order_books[self.symbol].bids
-            asks = order_books[self.symbol].asks
+        best_price = None
+
+        for symbol in order_books:
+            bids = order_books[symbol].bids
+            asks = order_books[symbol].asks
 
             if total_holding > 0:
-                if best_price is None or asks[-1].price > best_price[1]:
+                if len(asks) > 0 and (
+                    best_price is None or asks[-1].price < best_price[1]
+                ):
                     best_price = symbol, asks[-1].price, asks[-1].size
             else:
-                if best_price is None or bids[-1].price < best_price[1]:
+                if len(bids) > 0 and (
+                    best_price is None or bids[-1].price > best_price[1]
+                ):
                     best_price = symbol, bids[-1].price, bids[-1].size
 
-        if total_holding > 0:
-            self.bid(
-                price=best_price[1] + 5 * TICK_SIZE,
-                size=best_price[2] * 2,
-                symbol=best_price[0],
-                frames_to_expire=5,
+        if best_price is not None:
+            if total_holding < 0:
+                self.bid(
+                    price=best_price[1] + 5 * TICK_SIZE,
+                    size=min(best_price[2], -total_holding),
+                    symbol=best_price[0],
+                    frames_to_expire=self.resting_order_expiration_time,
+                )
+            else:
+                self.ask(
+                    price=best_price[1] - 5 * TICK_SIZE,
+                    size=min(best_price[2], total_holding),
+                    symbol=best_price[0],
+                    frames_to_expire=self.resting_order_expiration_time,
+                )
+
+    def update_fair_value(self):
+        order_books = self.exchange.public_info()
+        total_holding = self.get_total_holding()
+
+        mult = 1
+
+        if total_holding != 0:
+            mult = (
+                self.holding_adjustment_mult_range / (1 + np.exp(total_holding / 5))
+                + 1
+                - self.holding_adjustment_mult_range / 2
             )
-        else:
-            self.ask(
-                price=best_price[1] - 5 * TICK_SIZE,
-                size=best_price[2] * 2,
-                symbol=best_price[0],
-                frames_to_expire=5,
-            )
+
+        for symbol in order_books:
+            fair_estimators = []
+
+            bids = order_books[symbol].bids
+            asks = order_books[symbol].asks
+
+            if len(bids) > 0 and len(asks) > 0:
+                bid_prices = np.array([bid.price for bid in bids])
+                bid_sizes = np.array([bid.size for bid in bids])
+                ask_prices = np.array([ask.price for ask in asks])
+                ask_sizes = np.array([ask.size for ask in asks])
+
+                spread = ask_prices[-1] - bid_prices[-1]
+
+                mid = (bid_prices[-1] + ask_prices[-1]) / 2
+                fair_estimators.append(mid)
+
+                buy_side_mean = np.mean(bid_prices[-5:])
+                buy_side_volume = np.sum(bid_sizes[-5:])
+
+                sell_side_mean = np.mean(ask_prices[-5:])
+                sell_side_volume = np.sum(ask_sizes[-5:])
+
+                swmid = (
+                    buy_side_mean * sell_side_volume + sell_side_mean * buy_side_volume
+                ) / (buy_side_volume + sell_side_volume)
+                fair_estimators.append(swmid)
+
+                if spread <= self.tight_market_max_spread * TICK_SIZE:
+                    self.tight_market_mids[symbol].append(mid)
+
+            if len(self.tight_market_mids[symbol]) > 0:
+                self.tight_market_mids[symbol] = self.tight_market_mids[symbol][-20:]
+                tight_market_estimator = np.mean(self.tight_market_mids[symbol])
+                fair_estimators.append(tight_market_estimator)
+
+            if len(fair_estimators) > 0:
+                self.fairs[symbol] = np.mean(fair_estimators) * mult
+
+    def provide_liquidity(self):
+        order_books = self.exchange.public_info()
+
+        for symbol in order_books:
+            bids = order_books[symbol].bids
+            asks = order_books[symbol].asks
+
+            if len(bids) > 0 and len(asks) > 0:
+                bid_prices = np.array([bid.price for bid in bids])
+                ask_prices = np.array([ask.price for ask in asks])
+
+                spread = ask_prices[-1] - bid_prices[-1]
+
+                edge = max(spread * 0.4, self.min_edge)
+                if not bids[-1].id in self.open_orders:
+                    self.bid(
+                        price=self.clip(self.fairs[symbol] - edge),
+                        size=self.sizing,
+                        symbol=symbol,
+                        frames_to_expire=self.resting_order_expiration_time,
+                    )
+                if not asks[-1].id in self.open_orders:
+                    self.ask(
+                        price=self.clip(self.fairs[symbol] + edge),
+                        size=self.sizing,
+                        symbol=symbol,
+                        frames_to_expire=self.resting_order_expiration_time,
+                    )
+
+            else:
+                self.bid(
+                    price=0,
+                    size=self.sizing,
+                    symbol=symbol,
+                    frames_to_expire=self.resting_order_expiration_time,
+                )
+                self.ask(
+                    price=MAX_PAYOUT,
+                    size=self.sizing,
+                    symbol=symbol,
+                    frames_to_expire=self.resting_order_expiration_time,
+                )
 
     def update(self) -> None:
         super().update()
-
         if self.exchange.time_remaining < self.close_out_positions_at:
-            self.close_positions()
+            if self.exchange.time_remaining % 3 == 0:
+                self.close_positions()
         else:
-            pass
+            self.update_fair_value()
+            self.provide_liquidity()
